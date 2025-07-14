ae262c89131c455dd2724c2ed6bb49695015a062
JAIRO ALEJANDRO VALDOVINOS DAMIAN <22460706@colima.tecnm.mx>
2025-07-14 10:50:54 -0600

    hice los textos de los movimientos de la banda y agrege un sensor de flexion que indica ciertos datos

--- pryedcto_2/Jairo.cs
+++ pryedcto_2/Jairo.cs
@@ -1,11 +1,33 @@
 ﻿namespace pryedcto_2
 {
-    public class Jairo
+    public class ParteJairo
     {
-        public Jairo()
+        public float velocidadd;
+        public void Comenzar()
         {
-            // Constructor logic can be added here if needed
+            Console.WriteLine("Inicio del movimiento de la banda.");
+            velocidadd = 10.0f;
+            Console.WriteLine("La banda esta en movimiento: " + velocidadd);
+            velocidadd = 0.0f;
+            Console.WriteLine("La velocidad  esta disminullendo gradualmente.");
+            Console.WriteLine("Se a detenido por completo la banda");
+        }
+        public void flexion()
+        {
+            float flexion = velocidadd * 0.5f; // Ejemplo de cálculo de flexión
+
+            Console.WriteLine("La banda se encuentra en movimiento, no se puede flexionar.");
+            
+
+            Console.WriteLine($"la banda se ah detenido, calculando flexion....{flexion}");
+
+            Console.WriteLine("La flexion de la banda es....");
+           
         }
 
+
     }
+    
+
+
 }
