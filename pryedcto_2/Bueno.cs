49c359647ae5c48929e0f570d341526abca0dfa5
AlexHyva <abj1717@gmail.com>
2025-07-14 10:16:58 -0600

    Agrege todo

--- pryedcto_2/Bueno.cs
+++ pryedcto_2/Bueno.cs
@@ -1,7 +1,25 @@
-﻿namespace pryedcto_2
+﻿namespace Proyecto
 {
-    public class Bueno
+
+    //2.-Condicionar el objeto para poder moverlo a diferentes direcciones así como
+    //agregar o eliminar el mismo objeto para aumentar la cantidad del mismo
+    public class claseBUENO
     {
+        public float cajas;
+        public int direccion;
+        public class Mover
+        {
+
+            public void Mover1()
+            {
+                Console.WriteLine("Elije la direccion");
+                Console.WriteLine("Izquierda(1) o Derecha(0)");
+                int direccion = int.Parse(Console.ReadLine());
+                Console.WriteLine("Elije la cantidad de cajas");
+
+                float cajas = float.Parse(Console.ReadLine());
+            }
+        }
 
     }
 }
