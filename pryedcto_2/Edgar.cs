8748568e326ae22463356abfe31b99465986120d
Ramiro Valdovinos <edgaritoram2@gmail.com>
2025-07-14 10:26:41 -0600

    mi cambio en mi rama tiene un herror

--- pryedcto_2/Edgar.cs
+++ pryedcto_2/Edgar.cs
@@ -3,5 +3,41 @@
     public class Edgar
     {
 
+       
+            public Edgar()
+            {
+               
+            }
+            public void inicio()
+            {
+                Console.WriteLine("Hola jefe");
+            }
+            public void Mover1()
+            {
+                Console.WriteLine("El programa comenzará pronto");
+            }
+        
+
+        public class ensendido
+        {
+            public void ensender()
+            {
+                Console.WriteLine("La banda se ha encendido");
+            }
+            public ensendido()
+            {
+            
+            }
+        }
+
+        public class ErenciaEd : Edgar 
+        {
+            public void sobrecarga()
+            {
+              base.inicio();
+              base.Mover1();
+            }
+        }
     }
 }
+
--- pryedcto_2/Program.cs
+++ pryedcto_2/Program.cs
@@ -1,10 +1,20 @@
-﻿namespace pryedcto_2
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace pryedcto_2
 {
+    public class Instancia
+    {
+        public Edgar edgar = new Edgar();
+
+    }
    internal class Program
     {
+        public Edgar Edgar;
         static void Main(string[] args)
         {
-            Console.WriteLine("cambio2");
+            Edgar.ensendido encender = new Edgar.ensendido();
+            encender.ensender();
+
         }
     }
 }
