c0e06514a9c6c3c7092d92796ae87fe796310e6b
Ivanna Soto <Ivannasoga27@gmail.com>
2025-07-14 09:37:54 -0700

    fix: se arreglo el codigo base

--- pryedcto_2/Ivanna.cs
+++ pryedcto_2/Ivanna.cs
@@ -2,6 +2,39 @@
 {
     public class Ivanna
     {
+        public class Sensores
+        {
+            private claseBUENO _claseBUENO = new claseBUENO();
 
+            public bool detectarObjeto()
+            {
+                Console.WriteLine("Sensor: Se detectó objeto...");
+                return true;
+            }
+
+            public bool Velocidad()
+            {
+                Console.WriteLine("Sensor: Calculando velocidad...");
+                return true;
+            }
+
+            public void contador(float objetosDetectados)
+            {
+                float cajasActuales = _claseBUENO.cajas;
+
+                if (objetosDetectados == cajasActuales)
+                {
+                    Console.WriteLine("Sensor: Cantidad correcta de objetos.");
+                }
+                else if (objetosDetectados > cajasActuales)
+                {
+                    Console.WriteLine("Sensor: Hay demasiados objetos.");
+                }
+                else
+                {
+                    Console.WriteLine("Sensor: Faltan objetos.");
+                }
+            }
+
+        }
     }
-}
