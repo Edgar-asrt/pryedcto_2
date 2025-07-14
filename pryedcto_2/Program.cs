b713767dee84988bd113105fc2c860ee17cef7f7
Ramiro Valdovinos <edgaritoram2@gmail.com>
2025-07-14 08:59:42 -0600

    camboi

--- pryedcto_2/Program.cs
+++ pryedcto_2/Program.cs
@@ -1,5 +1,23 @@
 ﻿namespace pryedcto_2
 {
+    public class  Bueno
+    {
+        
+    }
+    public class Ivanna
+        {
+
+        }
+    public class Edgar
+    {
+
+    }
+    public class Jairo
+    {
+
+    }
+    
+    
     internal class Program
     {
         static void Main(string[] args)
