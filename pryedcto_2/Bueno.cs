3717fe35309d024cbe253e374be5ec5439a2bbf6
Ramiro Valdovinos <edgaritoram2@gmail.com>
2025-07-14 12:17:01 -0600

    Añadir clases y métodos para la lógica del motor
    Se ha añadido un constructor vacío a la clase `Bueno` y se ha renombrado a `Bueno1` con un nuevo constructor.
    
    En `Edgar.cs`, se han implementado nuevos métodos y una clase `ensendido` que hereda de `Edgar`.
    
    La clase `ErenciaEd` ahora llama a `MostrarDatos`, y se ha introducido la clase `Motor` con propiedades y un método para mostrar datos.
    
    En `Program.cs`, se ha creado una instancia de `Datos` y se han añadido métodos para interactuar con las clases, mejorando la lógica del programa.

--- pryedcto_2/Bueno.cs
+++ pryedcto_2/Bueno.cs
@@ -2,6 +2,8 @@
 {
     public class Bueno
     {
-
+        public Bueno()
+        {
+        }
     }
 }
--- pryedcto_2/Program.cs
+++ pryedcto_2/Program.cs
@@ -1,19 +1,35 @@
-﻿using System.Security.Cryptography.X509Certificates;
+﻿using System.Reflection.Metadata.Ecma335;
+using System.Security.Cryptography.X509Certificates;
 
 namespace pryedcto_2
 {
     public class Instancia
     {
         public Edgar edgar = new Edgar();
+        public Datos Datos = new Datos(100, 50, 20);  // ✅ ahora sí le das los parámetros requeridos
 
-    }
+
+        public void Edgar()
+        {
+            ensendido encender = new ensendido();
+            encender.ensender();
+        }
+        public void Edgar1()
+        {
+           Datos.MostrarDatos();
+
+        }
+
+}
    internal class Program
     {
         public Edgar Edgar;
+        public 
         static void Main(string[] args)
         {
-            Edgar.ensendido encender = new Edgar.ensendido();
-            encender.ensender();
+            Instancia instancia = new Instancia();
+            instancia.Edgar();
+            instancia.Edgar1();
 
         }
     }
